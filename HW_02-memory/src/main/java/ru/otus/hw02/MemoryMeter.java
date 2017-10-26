e91a401d93d088d5afa0cfe7000de05cacd3b05a
s-shumakov <ssshumakov@gmail.com>
2017-10-26 20:07:09 +0300

    HW_02

--- HW_02-memory/src/main/java/ru/otus/hw02/MemoryMeter.java
+++ HW_02-memory/src/main/java/ru/otus/hw02/MemoryMeter.java
@@ -10,29 +10,34 @@ public class MemoryMeter {
 
     public void getObjectSize(String name, Supplier<Object> supplier) throws InterruptedException {
         System.out.println("Calculate " + name + " size");
-        Runtime runtime = Runtime.getRuntime();
         System.gc();
-        long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        Object[] array = new Object[this.size];
+        long memoryBefore = getMemory();
         System.out.println("Memory before: " + memoryBefore);
         try {
-            initializeArray(supplier);
+            initializeArray(supplier, array);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
             e.printStackTrace();
         }
         System.out.println("Created " + this.size + " objects");
-        long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        long memoryAfter = getMemory();
         System.out.println("Memory after: " + memoryAfter);
         System.out.println(name + " size: " + (memoryAfter - memoryBefore) / this.size);
     }
 
-    public void initializeArray(Supplier<Object> supplier) throws IllegalAccessException, InstantiationException {
-        Object[] array = new Object[this.size];
+    public void initializeArray(Supplier<Object> supplier, Object[] array) throws IllegalAccessException, InstantiationException {
         if (supplier != null) {
             for (int i = 0; i < this.size; i++) {
                 array[i] = supplier.get();
             }
         }
     }
+
+    public long getMemory() {
+        Runtime runtime = Runtime.getRuntime();
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
+
 }
