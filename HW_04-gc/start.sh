e5270ae03d98e0c2df9b3ce7462d70f5ea8ff5e2
Sergey Shumakov <Sergey.Shumakov@rt.ru>
2017-11-09 12:14:15 +0300

    HW_04

--- HW_04-gc/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-MEMORY="-Xms51m -Xmx51m -XX:MaxMetaspaceSize=256m"
-GC="-XX:+UseConcMarkSweepGC"
-GC_LOG=" -verbose:gc -Xloggc:./logs/gc_pid_%p.log -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
-DUMP="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./dumps/"
-
-java $MEMORY $GC $GC_LOG $DUMP -jar target/HW_04-gc.jar > jvmStart.out
