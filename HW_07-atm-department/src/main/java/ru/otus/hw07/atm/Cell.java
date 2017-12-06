ad3b41493b72b8a567496c3793669576d0b279cd
s-shumakov <ssshumakov@gmail.com>
2017-12-07 00:33:15 +0300

    HW_07

---
+++ HW_07-atm-department/src/main/java/ru/otus/hw07/atm/Cell.java
@@ -0,0 +1,40 @@
+package ru.otus.hw07.atm;
+
+public class Cell {
+    public static final int MAX_AMOUNT = 2500;
+    private Denomination denomination;
+    private int amount;
+
+    public Cell(Denomination denomination) {
+        this.denomination = denomination;
+        this.amount = 0;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public Denomination getDenomination() {
+        return denomination;
+    }
+
+    public void putMoney(int amount) throws ATMException {
+        if (this.amount + amount > Cell.MAX_AMOUNT) {
+            throw new ATMException("Amount of money exceeded");
+        }
+        this.amount += amount;
+    }
+
+    public int takeMoney(int amount) throws ATMException {
+        if (this.amount - amount < 0) {
+            throw new ATMException("Insufficient money");
+        }
+        this.amount -= amount;
+        return amount;
+    }
+
+    public int getSumm() {
+        return this.amount * this.denomination.getValue();
+    }
+
+}
