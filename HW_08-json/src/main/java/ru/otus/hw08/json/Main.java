4d131f50f97e9e7f492edb6e3ccf2713cf5c6a97
Sergey Shumakov <Sergey.Shumakov@rt.ru>
2017-12-12 18:58:18 +0300

    HW_08

--- HW_08-json/src/main/java/ru/otus/hw08/json/Main.java
+++ HW_08-json/src/main/java/ru/otus/hw08/json/Main.java
@@ -3,9 +3,24 @@ package ru.otus.hw08.json;
 import com.google.gson.Gson;
 import org.json.simple.*;
 import javax.json.*;
+import java.lang.reflect.Field;
 
 public class Main {
     public static void main(String[] args) {
+        TestClass test = new TestClass();
+        test.firstName = "John";
+        test.lastName = "Smith";
+        test.age = 25;
+        test.address = new Address("21 2nd Street", "New York", "NY", 10021);
+        PhoneNumber[] phones = {new PhoneNumber("home", "212 555-1234"), new PhoneNumber("fax", "646 555-4567")};
+        test.phoneNumber = phones;
 
+        for (Field field : test.getClass().getFields()) {
+            try {
+                System.out.println(field.getName() + ": " + field.get(test));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ HW_08-json/src/main/java/ru/otus/hw08/json/TestClass.java
@@ -0,0 +1,10 @@
+package ru.otus.hw08.json;
+
+public class TestClass {
+    public String firstName;
+    public String lastName;
+    public int age;
+    public Address address;
+    public PhoneNumber[] phoneNumber;
+
+}
