5714a0b50cfe643909925816c3cd130bde72f319
Sergey Shumakov <Sergey.Shumakov@rt.ru>
2017-12-12 11:46:30 +0300

    HW_08

---
+++ HW_08-json/src/main/java/ru/otus/hw08/json/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.hw08.json;
+
+import com.google.gson.Gson;
+import org.json.simple.*;
+import javax.json.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
