d4327dc74f4b59fb38f2858080f19612247aee5f
s-shumakov <ssshumakov@gmail.com>
2017-12-18 00:46:13 +0300

    HW_08 fix

--- HW_08-json/src/main/java/ru/otus/hw08/json/JsonBuilder.java
+++ HW_08-json/src/main/java/ru/otus/hw08/json/JsonBuilder.java
@@ -30,79 +30,111 @@ public class JsonBuilder {
 
     @SuppressWarnings("unchecked")
     private void navigateTree(Object obj, JSONObject innerJsonObj) throws IllegalAccessException {
-        JSONArray jsonArray;
         for (Field field : obj.getClass().getDeclaredFields()) {
             field.setAccessible(true);
-            String type = getType(field);
+            FieldType type = getType(field);
             switch (type) {
-                case "Primitive":
-                case "String":
-                    System.out.println(type + " " + field.getName() + ": " + field.get(obj));
-                    if (field.get(obj) != null) {
-                        if (innerJsonObj != null) {
-                            innerJsonObj.put(field.getName(), field.get(obj));
-                        } else {
-                            jsonObject.put(field.getName(), field.get(obj));
-                        }
-                    }
+                case PRIMITIVE:
+                case STRING:
+                    putString(type.toString(), field, obj, innerJsonObj);
                     break;
-                case "ArrayOfObjects":
-                    jsonArray = new JSONArray();
-                    jsonObject.put(field.getName(), jsonArray);
-                    for (Object element : (Object[]) field.get(obj)) {
-                        JSONObject innerArrayObj = new JSONObject();
-                        System.out.println(type + " " + field.getName() + ": " + field.get(obj));
-                        jsonArray.add(innerArrayObj);
-                        navigateTree(element, innerArrayObj);
-                    }
+                case OBJECTSARRAY:
+                    putArrayOfObjects(type.toString(), field, obj);
                     break;
-                case "Array":
-                    jsonArray = new JSONArray();
-                    jsonObject.put(field.getName(), jsonArray);
-                    int length = Array.getLength(field.get(obj));
-                    for (int i = 0; i < length; i ++) {
-                        Object arrayElement = Array.get(field.get(obj), i);
-                        jsonArray.add(arrayElement);
-                        System.out.println(type + " " + field.getName() + ": " + arrayElement);
-                    }
+                case ARRAY:
+                    putArray(type.toString(), field, obj);
                     break;
-                case "Collection":
-                    jsonArray = new JSONArray();
-                    jsonObject.put(field.getName(), jsonArray);
-                    for (Object element : (Collection) field.get(obj)) {
-                        System.out.println(type + " " + field.getName() + ": " + element);
-                        jsonArray.add(element);
-                    }
+                case COLLECTION:
+                    putCollection(type.toString(), field, obj);
                     break;
-                case "Object":
-                    JSONObject innerObj = new JSONObject();
-                    jsonObject.put(field.getName(), innerObj);
-                    System.out.println(type + " " + field.getName() + ": " + field.get(obj));
-                    navigateTree(field.get(obj), innerObj);
+                case OBJECT:
+                    putObject(type.toString(), field, obj);
                     break;
             }
         }
     }
 
-    private String getType(Field field) {
+    private FieldType getType(Field field) {
         Class clazz = field.getType();
         System.out.println("clazz: " + clazz.getSimpleName());
         if (PRIMITIVES.contains(clazz)) {
-            return "Primitive";
+            return FieldType.PRIMITIVE;
         } else if (clazz == String.class) {
-            return "String";
+            return FieldType.STRING;
         } else if (clazz == Collection.class) {
-            return "Collection";
+            return FieldType.COLLECTION;
         } else if (clazz.isArray()) {
             if (PRIMITIVES.contains(clazz.getComponentType()) ||
                     clazz.getComponentType() == String.class) {
-                return "Array";
+                return FieldType.ARRAY;
             } else {
-                return "ArrayOfObjects";
+                return FieldType.OBJECTSARRAY;
             }
         } else {
-            return "Object";
+            return FieldType.OBJECT;
         }
     }
 
+    @SuppressWarnings("unchecked")
+    private void putString(String type, Field field, Object obj, JSONObject innerJsonObj) throws IllegalAccessException {
+        System.out.println(type + " " + field.getName() + ": " + field.get(obj));
+        if (field.get(obj) != null) {
+            if (innerJsonObj != null) {
+                innerJsonObj.put(field.getName(), field.get(obj));
+            } else {
+                jsonObject.put(field.getName(), field.get(obj));
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void putArrayOfObjects(String type, Field field, Object obj) throws IllegalAccessException {
+        JSONArray jsonArray = new JSONArray();
+        jsonObject.put(field.getName(), jsonArray);
+        for (Object element : (Object[]) field.get(obj)) {
+            JSONObject innerArrayObj = new JSONObject();
+            System.out.println(type + " " + field.getName() + ": " + field.get(obj));
+            jsonArray.add(innerArrayObj);
+            navigateTree(element, innerArrayObj);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void putArray(String type, Field field, Object obj) throws IllegalAccessException {
+        JSONArray jsonArray = new JSONArray();
+        jsonObject.put(field.getName(), jsonArray);
+        int length = Array.getLength(field.get(obj));
+        for (int i = 0; i < length; i ++) {
+            Object arrayElement = Array.get(field.get(obj), i);
+            jsonArray.add(arrayElement);
+            System.out.println(type + " " + field.getName() + ": " + arrayElement);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void putCollection(String type, Field field, Object obj) throws IllegalAccessException {
+        JSONArray jsonArray = new JSONArray();
+        jsonObject.put(field.getName(), jsonArray);
+        for (Object element : (Collection) field.get(obj)) {
+            System.out.println(type + " " + field.getName() + ": " + element);
+            jsonArray.add(element);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void putObject(String type, Field field, Object obj) throws IllegalAccessException {
+        JSONObject innerObj = new JSONObject();
+        jsonObject.put(field.getName(), innerObj);
+        System.out.println(type + " " + field.getName() + ": " + field.get(obj));
+        navigateTree(field.get(obj), innerObj);
+    }
+
+    private enum FieldType {
+        PRIMITIVE,
+        STRING,
+        COLLECTION,
+        ARRAY,
+        OBJECTSARRAY,
+        OBJECT
+    }
 }
